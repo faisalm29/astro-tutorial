49e1545e64ab329969efc51cdc7b972f24752717
faisalm29 <faisal.muhammad2911@gmail.com>
2025-10-07 14:37:30 +0700

    tutorial finised

---
+++ src/components/Greeting.jsx
@@ -0,0 +1,16 @@
+import { useState } from "preact/hooks";
+
+const Greeting = ({ message }) => {
+  const randomMessage = () =>
+    message[Math.floor(Math.random() * message.length)];
+  const [greeting, setGreeting] = useState(message[0]);
+
+  return (
+    <div>
+      <h3>{greeting}! Thank you for visiting!</h3>
+      <button onClick={() => setGreeting(randomMessage())}>New Greeting</button>
+    </div>
+  );
+};
+
+export default Greeting;
